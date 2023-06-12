d6daffa1d89aca9a038ad4e653579fda6d8edc65
黄浩楠 <huanghaonan@forensix.cn>
2023-06-12 13:11:17 +0800

    :white_check_mark: 添代码注释

--- routers/routers.go
+++ routers/routers.go
@@ -6,14 +6,16 @@ import (
 )
 
 func SetupRouter() *gin.Engine {
+	// 创建一个带有默认中间件的新的gin示例，包括Logger中间件和Recovery中间件
 	r := gin.Default()
 	// 告诉gin框架模板文件引用的静态文件去哪里找
 	r.Static("/static", "static")
 	// 告诉gin框架去哪里找模板文件
 	r.LoadHTMLGlob("templates/*")
+	// 为路由绑定处理函数
 	r.GET("/", controller.IndexHandler)
 
-	// v1
+	// 创建路由分组v1
 	v1Group := r.Group("v1")
 	{
 		// 待办事项
